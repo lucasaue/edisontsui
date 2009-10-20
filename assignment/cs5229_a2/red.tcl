d0c22ce4ddfc4a44c002f76a802736e512a59d18
edisontsui <edisontsui@62909508-e5d7-11dd-9ee4-63b70bd76fba>
2009-10-20 14:43:37 +0000

    ~ red script   - changable max threshold   - changable wq

--- assignment/cs5229_a2/red.tcl
+++ assignment/cs5229_a2/red.tcl
@@ -6,22 +6,28 @@
 # are a slightly-modified version of scripts from Davide Bergamasco at
 # Cisco.
 #
+# Modified by Edison
+# Added maxthresh & q_weight, refer to red.cc
 source common.tcl
 
 set minthresh [lindex $argv 0]
 set seed  [lindex $argv 1]
+set maxthresh [lindex $argv 2]
+set q_weight [lindex $argv 3]
+
 set queuetype RED
-set maxthresh [expr $minthresh * 3]
-puts "minthresh $minthresh maxthresh $maxthresh"
+#set maxthresh [expr $minthresh * 3]
+puts "minthresh $minthresh maxthresh $maxthresh q_weight $q_weight"
 set BuffSize 140
 Queue/RED set linterm_ 50 
 Queue/RED set thresh_ $minthresh
 Queue/RED set maxthresh_ $maxthresh
 Queue/RED set limit_ $BuffSize 
+Queue/RED set q_weight_ $q_weight
 init $seed $BuffSize
 topo
 make_traffic
-$ns at $SimTime "get_queue; finish"
+$ns at $SimTime "get_queue; finish" 
 puts "Running simulation $seed Buff= $BuffSize"
 $ns run
 
